ef60015de0709d1788a7da3974920cbc15dc0541
sonyaorekhova <orekhova_sonya@mail.ru>
2023-06-23 15:41:43 +0300

    1

---
+++ js.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    
+    function res(){
+        var width = $('html').width();
+          $('html').height(width);
+     } res();
+     
+     $( window ).resize(function() {
+         res();
+     });
+
+// ПОЯВЛЕНИЕ НАДПИСЕЙ
+    $(".hov1").hover(function (){
+        $(".dop1").toggleClass('dopop');
+      });
+      $(".hov2").hover(function (){
+        $(".dop2").toggleClass('dopop');
+      });
+      $(".hov3").hover(function (){
+        $(".dop3").toggleClass('dopop');
+      });
+      $(".hov4").hover(function (){
+        $(".dop4").toggleClass('dopop');
+      });
+      $(".hov5").hover(function (){
+        $(".dop5").toggleClass('dopop');
+      });  
+      $(".hov6").hover(function (){
+        $(".dop6").toggleClass('dopop');
+      });
+
+// ИНВЕРТ ЦВЕТА
+       
+});
+
